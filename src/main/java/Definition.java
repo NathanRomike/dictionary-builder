217c6884bc5eb067a62eb35057977d1c5a39b38c
NathanRomike <nathanromike@gmail.com>
2016-01-15 08:57:14 -0800

    Pass test to link input with definition: Green.

--- src/main/java/Definition.java
+++ src/main/java/Definition.java
@@ -1,11 +1,38 @@
 import java.util.*;
 
 public class Definition {
+  private static ArrayList<Definition> instances = new ArrayList<Definition>();
 
   private final String mUserInputDefinition;
+  private final Word mWord;
+  private int mId;
 
-  public Definition(String inputDefinition) {
+  public Definition(String inputDefinition, Word word) {
     mUserInputDefinition = inputDefinition;
+    mWord = word;
+    instances.add(this);
+    mId = instances.size();
+  }
+
+  public static ArrayList<Definition> all() {
+    return instances;
+  }
+
+  public static Definition find(int id) {
+    try {
+      return instances.get(id - 1);
+    } catch (IndexOutOfBoundsException abc) {
+      return null;
+    }
+  }
+
+  public String getDefinition() {
+    return mUserInputDefinition;
+  }
+
+
+  public Word getWord() {
+    return mWord;
   }
 
 
--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -5,7 +5,7 @@ public class DefinitionTest {
 
   @Test
   public void definition_initializesUserInputWord_true() {
-    Definition myDefinition = new Definition("A large, nocturnal, burrowing mammal, Orycteropus afer, of central and southern Africa.");
+    Definition myDefinition = new Definition("A large, nocturnal, burrowing mammal, Orycteropus afer, of central and southern Africa.", new Word("Aardvark"));
     assertEquals(true, myDefinition instanceof Definition);
   }
 
@@ -13,6 +13,6 @@ public class DefinitionTest {
   public void definition_hasDefinitionForAardvark() {
     Definition myDefinition = new Definition("A large, nocturnal, burrowing mammal, Orycteropus afer, of central and southern Africa.", new Word("Aardvark"));
     String expected = "A large, nocturnal, burrowing mammal, Orycteropus afer, of central and southern Africa.";
-    assertEquals(expected, definition.getDefinition());
+    assertEquals(expected, myDefinition.getDefinition());
   }
 }
