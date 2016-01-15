017184b57d765d81e266a52df0f3ec6ba3fffafb
NathanRomike <nathanromike@gmail.com>
2016-01-15 08:31:49 -0800

    Add test to link word and definition inputs to fail: Red.

--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -5,7 +5,14 @@ public class DefinitionTest {
 
   @Test
   public void definition_initializesUserInputWord_true() {
-    Definition myDefinition = new Definition("An animal which is the first word in most dictionaries.");
+    Definition myDefinition = new Definition("A large, nocturnal, burrowing mammal, Orycteropus afer, of central and southern Africa.");
     assertEquals(true, myDefinition instanceof Definition);
+  }
+
+  @Test
+  public void definition_hasDefinitionForAardvark() {
+    Definition myDefinition = new Definition("A large, nocturnal, burrowing mammal, Orycteropus afer, of central and southern Africa.", new Word("Aardvark"));
+    String expected = "A large, nocturnal, burrowing mammal, Orycteropus afer, of central and southern Africa.";
+    assertEquals(expected, definition.getDefinition());
   }
 }
