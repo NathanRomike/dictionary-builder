b03f8a583fd300ab25d8047676b484a4c090e55f
NathanRomike <nathanromike@gmail.com>
2016-01-15 09:57:50 -0800

    Pass integration test to confirm app is compliling and drafting home page.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,13 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
 
+    get("/", (reguest, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
